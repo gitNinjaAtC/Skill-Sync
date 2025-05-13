6e40a2c4e7634bf34ac3d5356d8c5df739b5ea65
Shivam774705 <kumarshivam774705@gmail.com>
2025-05-14 03:28:51 +0530

    Edit profile page

--- CLIENT_F/FRONTEND/src/pages/profile/Profile.jsx
+++ CLIENT_F/FRONTEND/src/pages/profile/Profile.jsx
@@ -3,7 +3,7 @@ import "./profile.scss";
 import {
   FacebookTwoTone as FacebookTwoToneIcon,
   LinkedIn as LinkedInIcon,
-  Instagram as InstagramIcon, 
+  Instagram as InstagramIcon,
   Twitter as TwitterIcon,
   Place as PlaceIcon,
   Language as LanguageIcon,
@@ -15,19 +15,17 @@ import { useContext } from "react";
 import { AuthContext } from "../../context/authContext";
 
 const Profile = () => {
-  const { id } = useParams(); // Access the dynamic id from the URL
+  const { id } = useParams();
   const { currentUser } = useContext(AuthContext);
 
   return (
     <div className="profile">
       <div className="images">
-        {/* Display cover and profile images */}
         <img
           src="https://images.pexels.com/photos/13440765/pexels-photo-13440765.jpeg"
-          alt=""
+          alt="Cover"
           className="cover"
         />
-        {/* If profilePic is available, use it, otherwise fallback to default image */}
         <img
           src={currentUser?.profilePic || "/default-profile.jpg"}
           alt="Profile"
@@ -37,29 +35,38 @@ const Profile = () => {
       <div className="profileContainer">
         <div className="uInfo">
           <div className="left">
-            {/* Added links to social media */}
-            <a href="http://facebook.com"><FacebookTwoToneIcon fontSize="large" /></a>
-            <a href="http://instagram.com"><InstagramIcon fontSize="large" /></a>
-            <a href="http://twitter.com"><TwitterIcon fontSize="large" /></a>
-            <a href="http://linkedin.com"><LinkedInIcon fontSize="large" /></a>
-            
+            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+              <FacebookTwoToneIcon fontSize="large" />
+            </a>
+            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+              <InstagramIcon fontSize="large" />
+            </a>
+            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+              <TwitterIcon fontSize="large" />
+            </a>
+            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+              <LinkedInIcon fontSize="large" />
+            </a>
           </div>
           <div className="center">
-            {/* Display current user's name, or "Guest" if no name available */}
             <span>{currentUser?.name || "Guest"}</span>
             <div className="info">
-              {/* Display user's country and language */}
-              <div className="item"><PlaceIcon /><span>India</span></div>
-              <div className="item"><LanguageIcon /><span>English</span></div>
+              <div className="item">
+                <PlaceIcon />
+                <span>India</span>
+              </div>
+              <div className="item">
+                <LanguageIcon />
+                <span>English</span>
+              </div>
             </div>
-            <button>Follow</button>
+            <button>Message</button>
           </div>
           <div className="right">
             <EmailOutlinedIcon />
             <MoreVertIcon />
           </div>
         </div>
-        {/* Rendering Posts component */}
         <Posts />
       </div>
     </div>
