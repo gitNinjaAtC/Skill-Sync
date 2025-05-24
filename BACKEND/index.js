60abf47ebcb7ae9efe781bfebb4b6ccc7f3b5eae
Purushottam Kumar Mandal <122455904+gitNinjaAtC@users.noreply.github.com>
2025-05-25 00:30:39 +0530

    Update index.js

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -24,7 +24,7 @@ db();
 // CORS configuration
 app.use(
   cors({
-    origin: "https://skill-sync-backend-522o.onrender.com",
+    origin: "https://skill-sync-frontend.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
